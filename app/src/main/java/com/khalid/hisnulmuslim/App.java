2f58041ed1ee6146697433a2231c34924b414684
Khalid <mohammad.khalid.hussain@gmail.com>
2015-08-19 04:16:11 +0800

    Preparation to update Android Iconics library

---
+++ app/src/main/java/com/khalid/hisnulmuslim/App.java
@@ -0,0 +1,21 @@
+package com.khalid.hisnulmuslim;
+
+import android.app.Application;
+
+/*import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.Iconics;*/
+
+/**
+ * Created by Khalid on 19 أغسطس.
+ */
+
+// This file might be required once I need to update the Android Iconics library (https://github.com/mikepenz/Android-Iconics)
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Iconics.registerFont(new FontAwesome());
+        // Iconics.registerFont(new GoogleMaterial());
+    }
+}
