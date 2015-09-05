2a8e48f63e98ac5da1f25b5dee5f228a0648c353
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2015-09-05 10:43:21 +0800

    Cleaning out branch

--- app/src/main/java/com/khalid/hisnulmuslim/loader/DuaGroupLoader.java
+++ app/src/main/java/com/khalid/hisnulmuslim/loader/DuaGroupLoader.java
@@ -1,6 +1,7 @@
 package com.khalid.hisnulmuslim.loader;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -9,21 +10,36 @@ import com.khalid.hisnulmuslim.model.Dua;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class DuaGroupLoader extends AbstractQueryLoader<List<Dua>> {
+
     public DuaGroupLoader(Context context) {
         super(context);
     }
+    public Locale deviceLocale;
+    public String groupTitleLanguage;
 
     @Override
     public List<Dua> loadInBackground() {
+
+        Locale mArabicLocale = new Locale("ar_EG", "ar_IL");
+
+        deviceLocale = Resources.getSystem().getConfiguration().locale;
+
+        if (deviceLocale.equals(Locale.ENGLISH))
+            groupTitleLanguage = HisnDatabaseInfo.DuaGroupTable.ENGLISH_TITLE;
+        else if (deviceLocale.equals(mArabicLocale))
+            groupTitleLanguage = HisnDatabaseInfo.DuaGroupTable.ARABIC_TITLE;
+
         List<Dua> results = null;
         Cursor duaGroupCursor = null;
         try {
             final SQLiteDatabase database = mDbHelper.getDb();
             duaGroupCursor = database.query(HisnDatabaseInfo.DuaGroupTable.TABLE_NAME,
                     new String[]{HisnDatabaseInfo.DuaGroupTable._ID,
-                            HisnDatabaseInfo.DuaGroupTable.ENGLISH_TITLE},
+                            groupTitleLanguage},
+                            // HisnDatabaseInfo.DuaGroupTable.ENGLISH_TITLE},
                     null,
                     null,
                     null,
